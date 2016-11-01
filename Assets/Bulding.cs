fd4d5e53a050560eaa141105799c3679bf7efe3c
Alexandre FREARD <u54137392@holyowlstudio.com>
2016-11-01 18:01:52 +0100

    First Commit

---
+++ Assets/Bulding.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bulding : MonoBehaviour {
+    public float factorSpeed = 0.3f;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 tmp = transform.position;
+        tmp.z -= factorSpeed;
+        transform.position = tmp;
+	}
+}
---
+++ Assets/WorldManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WorldManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        Vector3 tmp = col.transform.position;
+        tmp.z = 32;
+        tmp.y = Random.Range(-1f, 1f);
+        col.transform.position = tmp;
+    }
+}
